e454c5a9ccbeea98907071a632b45f1ccb23abc6
PianPir <skirr.den@gmail.com>
2026-01-25 00:52:21 +0300

    Initial commit

---
+++ src/main/java/io/github/pianpir/jbillcore/JbillcoreApplication.java
@@ -0,0 +1,13 @@
+package io.github.pianpir.jbillcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbillcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbillcoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/pianpir/jbillcore/JbillcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pianpir.jbillcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbillcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
